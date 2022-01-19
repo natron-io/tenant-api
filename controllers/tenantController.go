4c2bab693467a70425298aa4ef6799b12926eaea
Jan Lauber <jan.lauber@protonmail.ch>
2022-01-19 23:58:54 +0100

    add namespaces and serviceaccounts
    Signed-off-by: Jan Lauber <jan.lauber@protonmail.ch>

--- controllers/tenantController.go
+++ controllers/tenantController.go
@@ -9,12 +9,45 @@ import (
 )
 
 func GetPods(c *fiber.Ctx) error {
-	// Get all pods in all namespaces
+	// Get all pods in the cluster
 	pods, err := util.Clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
+
+	// // Get all pods in all namespaces by label
+	// pods, err := util.Clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{LabelSelector: "app=tenant-api"})
+
+	// // Get pvc by label
+	// pvc, err := util.Clientset.CoreV1().PersistentVolumeClaims("").List(context.TODO(), metav1.ListOptions{LabelSelector: "tenant=tenant-api"})
+
+	// // Get cpu request by label
+	// cpuRequest, err := util.Clientset.CoreV1().ConfigMaps("").List(context.TODO(), metav1.ListOptions{LabelSelector: "tenant=tenant-api"})
+
 	util.InfoLogger.Println("/api/v1/pods hit from IP: " + c.IP())
 	if err != nil {
 		util.WarningLogger.Println(err.Error())
 	}
-	// Return pods as JSON
-	return c.JSON(pods)
+
+	return c.JSON(pods.Items)
+
+}
+
+func GetNamespaces(c *fiber.Ctx) error {
+	namespaces, err := util.Clientset.CoreV1().Namespaces().List(context.TODO(), metav1.ListOptions{})
+
+	util.InfoLogger.Println("/api/v1/namespaces hit from IP: " + c.IP())
+	if err != nil {
+		util.WarningLogger.Println(err.Error())
+	}
+
+	return c.JSON(namespaces.Items)
+}
+
+func GetServiceAccounts(c *fiber.Ctx) error {
+	serviceAccounts, err := util.Clientset.CoreV1().ServiceAccounts("").List(context.TODO(), metav1.ListOptions{})
+
+	util.InfoLogger.Println("/api/v1/serviceAccounts hit from IP: " + c.IP())
+	if err != nil {
+		util.WarningLogger.Println(err.Error())
+	}
+
+	return c.JSON(serviceAccounts.Items)
 }
